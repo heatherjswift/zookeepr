bb1e22748f25bbd15dbf2b49cc1e7e6ce62fc597
Hede <Heatherj.swift@gmail.com>
2021-04-29 21:22:19 -0400

    closes #5; build dataset for zookeepers

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const animalRoutes = require('../apiRoutes/animalRoutes');
+const zookeeperRoutes = require('../apiRoutes/zookeeperRoutes');
+
+router.use(animalRoutes);
+router.use(zookeeperRoutes);
+// or you can router.use(require('./zookeeperRoutes'));
+
+module.exports = router;
